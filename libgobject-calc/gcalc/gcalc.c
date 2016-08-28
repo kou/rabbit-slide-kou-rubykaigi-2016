21204ce7b7dc1b4b8f103486b4093d9f41717130
Kouhei Sutou <kou@clear-code.com>
2016-08-28 13:33:45 +0900

    Add GObject Introspection based bindings

---
+++ libgobject-calc/gcalc/gcalc.c
@@ -0,0 +1,22 @@
+/* -*- Mode: C; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include "gcalc.h"
+
+/**
+ * gcalc_add:
+ * @x: Adder.
+ * @y: Addend.
+ *
+ * Adds two integers.
+ *
+ * Returns: Added integer.
+ */
+gint
+gcalc_add(gint x, gint y)
+{
+    return x + y;
+}
---
+++ libgobject-calc/gcalc/gcalc.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <glib.h>
+
+gint gcalc_add(gint x, gint y);
