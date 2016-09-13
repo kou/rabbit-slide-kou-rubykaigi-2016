5b159a56b3b6220c672e0616f0013603c10bcc22
Kouhei Sutou <kou@clear-code.com>
2016-09-13 18:28:55 +0900

    Add error check

--- c-api/bindings/extconf.rb
+++ c-api/bindings/extconf.rb
@@ -1,7 +1,7 @@
 require "mkmf"
 
 dir_config("libhello")
-have_header("hello.h")
-have_library("hello", "hello_new")
+exit(false) unless have_header("hello.h")
+exit(false) unless have_library("hello", "hello_new")
 
 create_makefile("hello")
