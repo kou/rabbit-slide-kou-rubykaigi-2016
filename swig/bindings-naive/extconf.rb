1442031975bbcedea85f2365779a67d962576cc3
Kouhei Sutou <kou@clear-code.com>
2016-09-14 14:49:39 +0900

    swig: improve extconf.rb to support running swig

--- swig/bindings-naive/extconf.rb
+++ swig/bindings-naive/extconf.rb
@@ -1,7 +1,15 @@
 require "mkmf"
 
-dir_config("libhello")
-have_header("hello.h")
-have_library("hello", "hello_new")
+include_dir, library_dir = dir_config("libhello")
+exit(false) unless have_header("hello.h")
+exit(false) unless have_library("hello", "hello_new")
+
+$srcs = ["hello_wrap.c"]
 
 create_makefile("hello")
+File.open("Makefile", "a") do |makefile|
+  makefile.puts(<<-MAKEFILE)
+hello_wrap.c: hello.i
+	swig -Wall -I"#{include_dir}" -ruby -o $@ $<
+  MAKEFILE
+end
